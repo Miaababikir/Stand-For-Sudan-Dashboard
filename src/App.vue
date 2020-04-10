df45a90f940f0545465b9a5c5fdd69c9bd737d92
Mosab Ibrahim <miaababikir@gmail.com>
2020-04-10 06:39:35 +0200

    Adding google analytics

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
             </div>
             <footer class="bg-white">
                 <div class="px-8 py-6 text-gray-800 text-center">
-                    <p>Built with <span class="text-red-600">❤️</span> By @<a class="text-blue-600" href="https://www.facebook.com/OmegaTeamSD">Omega</a> - Hosted by @<a class="text-blue-600" href="https://twitter.com/obayhamid">Obiefy</a></p>
+                    <p>Built with <span class="text-red-600">❤️</span> By @<a class="text-blue-600" href="https://www.facebook.com/OmegaTeamSD">Omega</a> - And @<a class="text-blue-600" href="https://twitter.com/obayhamid">Obiefy</a></p>
                 </div>
             </footer>
         </div>
@@ -29,6 +29,9 @@
     export default {
         name: 'App',
         components: {HeaderComponent, CardList, LineChartComponent},
+        mounted() {
+            this.$ga.page('/');
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueAnalytics from 'vue-analytics';
+
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-161702225-2'
+});
 
 new Vue({
   render: h => h(App),
